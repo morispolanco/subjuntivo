29b323c3cdb59e9fe9a71604e5ea7978deefba5b
morispolanco <morispolanco@gmail.com>
2023-09-27 17:13:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,8 @@
 import streamlit as st
-import spacy.cli
-
-def descargar_modelo():
-    spacy.cli.download("es_core_news_sm")
-    st.write("El modelo se ha descargado correctamente.")
+import spacy
 
 def cambiar_conjugaciones(texto):
-    nlp = spacy.load("es_core_news_sm")
+    nlp = spacy.load("es_core_news_sm", disable=["parser", "ner"])
     doc = nlp(texto)
     resultado = []
     for token in doc:
@@ -19,8 +15,6 @@ def cambiar_conjugaciones(texto):
 def main():
     st.title("Cambiador de Conjugaciones")
     texto = st.text_area("Ingresa el texto semiespañol:")
-    if st.button("Descargar modelo"):
-        descargar_modelo()
     if st.button("Cambiar conjugaciones"):
         resultado = cambiar_conjugaciones(texto)
         st.write("Texto corregido:")

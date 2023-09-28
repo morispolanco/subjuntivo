d1d561b0980b6e0e553105cee55b30bc0088c610
morispolanco <morispolanco@gmail.com>
2023-09-28 07:11:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,29 @@
-import streamlit as st
+import spacy
 import re
 
-def extract_subjunctive_verbs(text):
-  """Extracts verbs in the subjunctive mode from a Spanish text.
+nlp = spacy.load("es_core_news_sm")
+
+def extract_subjunctive_verbs_from_file(filename):
+  """Extracts subjunctive verbs from a Spanish text file.
 
   Args:
-    text: A Spanish text.
+    filename: The path to the Spanish text file.
 
   Returns:
-    A list of subjunctive verbs in the text.
+    A list of subjunctive verbs.
   """
 
-  subjunctive_verb_regex = re.compile(r'\b(que|ojalá|quizá(s)|tal vez|acaso|espero|quiero|deseo|necesito|es importante que|es necesario que|es mejor que|es preferible que|es recomendable que|es obligatorio que|es aconsejable que|es posible que|es probable que|es dudoso que|es difícil que|es fácil que|no|nunca)\s+(sea|seas|sea|seamos|sean|estemos|estén|esté|fuera|fueras|fuera|fuéramos|fueran|estuviera|estuvieras|estuviera|estuviéramos|estuvieran|haya|hayas|haya|hayamos|hayan|hubiera|hubieras|hubiera|hubiéramos|hubieran|vengamos|estemos|estén|estemos)\s+\w+\b', re.IGNORECASE)
   subjunctive_verbs = []
-  for match in subjunctive_verb_regex.finditer(text):
-    verb = match.group(3)
-    # Remove the trailing infinitive marker, if any.
-    verb = verb.rstrip('r')
-    # Add the verb to the list, if it is not already there.
-    if verb not in subjunctive_verbs:
-      subjunctive_verbs.append(verb)
+  with open(filename, "r", encoding="utf-8") as f:
+    text = f.read()
+    doc = nlp(text)
+    for token in doc:
+      if token.pos == "VERB" and token.tag == "SUBJ":
+        subjunctive_verbs.append(token.text)
   return subjunctive_verbs
 
-def main():
-  """Renders the Streamlit app."""
+filename = "spanish_text.txt"
 
-  text = st.text_area('Enter a Spanish text:')
-  if text:
-    subjunctive_verbs = extract_subjunctive_verbs(text)
-    if subjunctive_verbs:
-      st.write('\n## Subjunctive verbs:')
-      for verb in subjunctive_verbs:
-        st.write(verb)
-    else:
-      st.write('No subjunctive verbs found.')
+subjunctive_verbs = extract_subjunctive_verbs_from_file(filename)
 
-if __name__ == '__main__':
-  main()
+print(subjunctive_verbs)

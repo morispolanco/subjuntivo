37f7a8c8029eca9ada733262765f16ff901a3e35
morispolanco <morispolanco@gmail.com>
2023-09-27 19:37:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
-
 import streamlit as st
 import spacy
-import csv
 
 def encontrar_verbos_subjuntivo(texto):
     nlp = spacy.load("es_core_news_sm")
@@ -10,17 +8,9 @@ def encontrar_verbos_subjuntivo(texto):
     for token in doc:
         if token.pos_ == "VERB" and "Sub" in token.morph.get("Mood", ""):
             verbos_subjuntivo.append(token.text)
+        elif token.pos_ == "AUX" and "Sub" in token.morph.get("Mood", ""):
+            verbos_subjuntivo.append(token.text)
     return verbos_subjuntivo
-
-def exportar_a_csv(verbos_subjuntivo):
-    with open("resultados.csv", "w", newline="") as archivo_csv:
-        writer = csv.writer(archivo_csv)
-        writer.writerow(["Verbo", "Frecuencia"])
-        frecuencias = {}
-        for verbo in verbos_subjuntivo:
-            frecuencias[verbo] = frecuencias.get(verbo, 0) + 1
-        for verbo, frecuencia in frecuencias.items():
-            writer.writerow([verbo, frecuencia])
 
 def main():
     st.title("Buscador de Verbos en Modo Subjuntivo")
@@ -31,8 +21,6 @@ def main():
             st.write("Verbos en modo subjuntivo encontrados:")
             for verbo in verbos_subjuntivo:
                 st.write(verbo)
-            exportar_a_csv(verbos_subjuntivo)
-            st.write("Resultados exportados a resultados.csv")
         else:
             st.write("No se encontraron verbos en modo subjuntivo.")
 

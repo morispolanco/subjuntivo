4b2efd676abe2af5fb8b183ea77d30ad4ef8c1ac
morispolanco <morispolanco@gmail.com>
2023-09-28 07:05:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def extract_subjunctive_verbs(text):
     A list of subjunctive verbs in the text.
   """
 
-  subjunctive_verb_regex = re.compile(r'\b(que|ojalá|quizá(s)|tal vez|acaso|espero|quiero|deseo|necesito|es importante que|es necesario que|es mejor que|es preferible que|es recomendable que|es obligatorio que|es aconsejable que|es posible que|es probable que|es dudoso que|es difícil que|es fácil que|no|nunca)\s+(sea|seas|sea|seamos|sean|estemos|estén|esté|fuera|fueras|fuera|fuéramos|fueran|estuviera|estuvieras|estuviera|estuviéramos|estuvieran|haya|hayas|haya|hayamos|hayan|hubiera|hubieras|hubiera|hubiéramos|hubieran)\s+\w+\b', re.IGNORECASE)
+  subjunctive_verb_regex = re.compile(r'\b(que|ojalá|quizá(s)|tal vez|acaso|espero|quiero|deseo|necesito|es importante que|es necesario que|es mejor que|es preferible que|es recomendable que|es obligatorio que|es aconsejable que|es posible que|es probable que|es dudoso que|es difícil que|es fácil que|no|nunca)\s+(sea|seas|sea|seamos|sean|estemos|estén|esté|fuera|fueras|fuera|fuéramos|fueran|estuviera|estuvieras|estuviera|estuviéramos|estuvieran|haya|hayas|haya|hayamos|hayan|hubiera|hubieras|hubiera|hubiéramos|hubieran|vengamos|estemos|estén|estemos)\s+\w+\b', re.IGNORECASE)
   subjunctive_verbs = []
   for match in subjunctive_verb_regex.finditer(text):
     verb = match.group(3)
@@ -25,17 +25,10 @@ def extract_subjunctive_verbs(text):
 def main():
   """Renders the Streamlit app."""
 
-  st.title('Spanish Subjunctive Verb Extractor')
-
   text = st.text_area('Enter a Spanish text:')
   if text:
     subjunctive_verbs = extract_subjunctive_verbs(text)
     if subjunctive_verbs:
       st.write('\n## Subjunctive verbs:')
       for verb in subjunctive_verbs:
-        st.write(verb)
-    else:
-      st.write('No subjunctive verbs found.')
-
-if __name__ == '__main__':
-  main()
+        st.

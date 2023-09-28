e71bf6152faa8be1cca042920ab0e8968c53019a
morispolanco <morispolanco@gmail.com>
2023-09-28 05:28:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,26 @@
 import streamlit as st
 import spacy
 
-def encontrar_verbos_subjuntivo(texto):
+def buscar_subjuntivos(texto):
     nlp = spacy.load("es_core_news_sm")
     doc = nlp(texto)
-    verbos_subjuntivo = []
-    for token in doc:
-        if token.pos_ == "VERB" and "Sub" in token.morph.get("Mood", ""):
-            verbos_subjuntivo.append(token.text)
-        elif token.pos_ == "AUX" and "Sub" in token.morph.get("Mood", ""):
-            verbos_subjuntivo.append(token.text)
-    return verbos_subjuntivo
+    subjuntivos = [token.text for token in doc if token.pos_ == "VERB" and token.mood == "SUBJUNCTIVE"]
+    return subjuntivos
 
 def main():
-    st.title("Buscador de Verbos en Modo Subjuntivo")
-    texto = st.text_area("Ingresa el texto:")
-    if st.button("Buscar verbos en modo subjuntivo"):
-        verbos_subjuntivo = encontrar_verbos_subjuntivo(texto)
-        if verbos_subjuntivo:
-            st.write("Verbos en modo subjuntivo encontrados:")
-            for verbo in verbos_subjuntivo:
-                st.write(verbo)
+    st.title("Aplicación de búsqueda de subjuntivos")
+    st.write("Ingrese un texto para buscar subjuntivos")
+
+    texto = st.text_area("Texto")
+
+    if st.button("Buscar"):
+        subjuntivos = buscar_subjuntivos(texto)
+        if subjuntivos:
+            st.write("Subjuntivos encontrados:")
+            for subjuntivo in subjuntivos:
+                st.write(subjuntivo)
         else:
-            st.write("No se encontraron verbos en modo subjuntivo.")
+            st.write("No se encontraron subjuntivos en el texto")
 
 if __name__ == "__main__":
     main()

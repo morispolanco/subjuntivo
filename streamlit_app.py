42a88e751600a2ebf2698ab9862614d6938f84af
morispolanco <morispolanco@gmail.com>
2023-09-28 03:03:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,20 @@ import spacy
 nlp = spacy.load("es_core_news_sm")
 
 # Título de la aplicación
-st.title("Localizador de verbos en subjuntivo")
+st.title("Extracción de verbos en subjuntivo en español")
 
 # Texto de entrada
 text = st.text_area("Ingrese el texto en español:", height=200)
 
-# Botón para analizar el texto
-if st.button("Analizar"):
+# Botón para extraer los verbos en subjuntivo
+if st.button("Extraer verbos en subjuntivo"):
     # Procesar el texto con Spacy
     doc = nlp(text)
 
-    # Encontrar los verbos en subjuntivo
-    subjunctive_verbs = [token.text for token in doc if token.pos_ == "VERB" and token.mood == "SUBJ"]
+    # Encontrar los verbos en subjuntivo en el texto
+    verbos_subjuntivo = [token.text for token in doc if token.pos_ == "VERB" and token.mood == "SUBJ"]
 
-    # Mostrar los resultados
-    if subjunctive_verbs:
-        st.success("Se encontraron los siguientes verbos en subjuntivo:")
-        for verb in subjunctive_verbs:
-            st.write(verb)
-    else:
-        st.info("No se encontraron verbos en subjuntivo en el texto.")
+    # Mostrar los verbos en subjuntivo encontrados
+    st.subheader("Verbos en subjuntivo encontrados:")
+    for verbo in verbos_subjuntivo:
+        st.write(verbo)

bb249e4cae6befd8e601732a2c3ed07f6725ec80
morispolanco <morispolanco@gmail.com>
2023-09-29 04:32:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,64 +1,27 @@
 import streamlit as st
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
+import spacy
+from spacy_conjugator import SpacyConjugator
 
-# Download required NLTK data
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
+@st.cache(allow_output_mutation=True) 
+def load_model():
+    nlp = spacy.load("es_core_news_md")
+    conjugator = SpacyConjugator(nlp)
+    return nlp, conjugator
 
-# Define a function to find verbs in subjunctive mood
-def find_subjunctive_verbs(text):
-    # Tokenize the text
-    tokens = word_tokenize(text)
-    
-    # Perform part-of-speech tagging
-    pos_tags = nltk.pos_tag(tokens)
-    
-    # Initialize the lemmatizer
-    lemmatizer = WordNetLemmatizer()
-    
-    # Filter out non-verbs
-    verbs = [(word, pos) for word, pos in pos_tags if pos.startswith('V')]
-    
-    # Filter out verbs not in the subjunctive mood
-    subjunctive_verbs = []
-    for word, pos in verbs:
-        # Use WordNet to find synonyms of the verb
-        synonyms = wordnet.synsets(word, pos=get_wordnet_pos(pos))
-        if synonyms:
-            synonym = synonyms[0]
-            lemma = lemmatizer.lemmatize(word, get_wordnet_pos(pos))
-            # Check if the verb is in the subjunctive mood
-            if lemma in synonym.lemmas()[0].name().split('/'):
-                subjunctive_verbs.append((word, pos))
-    
-    return subjunctive_verbs
+nlp, conjugator = load_model() 
 
-# Function to get the WordNet part-of-speech
-def get_wordnet_pos(nltk_pos):
-    if nltk_pos.startswith('J'):
-        return wordnet.ADJ
-    elif nltk_pos.startswith('V'):
-        return wordnet.VERB
-    elif nltk_pos.startswith('N'):
-        return wordnet.NOUN
-    elif nltk_pos.startswith('R'):
-        return wordnet.ADV
+st.title("Find Verbs in Subjunctive Mode")
+
+text = st.text_area("Enter Text in Spanish", "Ella quiso que yo viniera a la fiesta.")
+
+if text:
+    doc = nlp(text)
+    verbs = []
+    for token in doc:
+        if token.pos_ == "VERB" and conjugator.conjugate(token, tense="present", mood="subjunctive"):
+            verbs.append(token.text)
+    
+    if verbs:
+        st.write("Verbs in subjunctive mode found:", ", ".join(verbs))
     else:
-        return wordnet.NOUN
-
-def main():
-    st.title("Verbs in Subjunctive Mood Finder")
-    text = st.text_area("Enter a sentence or paragraph:")
-    
-    if st.button("Find Verbs in Subjunctive Mood"):
-        subjunctive_verbs = find_subjunctive_verbs(text)
-        st.write("Verbs in Subjunctive Mood:")
-        for verb, pos in subjunctive_verbs:
-            st.write(f"{verb} ({pos})")
-
-if __name__ == "__main__":
-    main()
+        st.write("No verbs in subjunctive mode found.")

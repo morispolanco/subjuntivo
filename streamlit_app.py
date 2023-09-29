2ffb9b185d912502b00ca617cbdfbed68dee2fea
morispolanco <morispolanco@gmail.com>
2023-09-29 17:43:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,14 @@
 import streamlit as st
 import spacy
 
+nlp = spacy.load("es_core_news_sm")
 
-# Cargar el modelo de lenguaje SpaCy
-nlp = spacy.load('es_core_news_sm')
-
-
-
-def extract_verbs_subjunctive(text):
+def get_verbs(text):
     doc = nlp(text)
-    verbs = []
-    subjunctive_verbs = []
-    for token in doc:
-        if token.pos_ == 'VERB':
-            verbs.append(token.text)
-            if token.morph.get('VerbForm') == 'Sub':
-                subjunctive_verbs.append(token.text)
-    return verbs, subjunctive_verbs
+    verbs = [token.lemma_ for token in doc if token.pos_ == "VERB" and token.tag_ == "VS"]
+    return verbs
 
-
-def app():
-    st.title('Verbo y Modo Subjuntivo Extractor')
-    st.write('Introduce el texto a analizar:')
-    text = st.text_input('')
-    verbs, subjunctive_verbs = extract_verbs_subjunctive(text)
-
-    st.write('Verbos: ', verbs)
-    st.write('Verbos en modo subjuntivo: ', subjunctive_verbs)
-
-
-if __name__ == "__main__":
-    app()
+st.title("Subjunctive Verbs Extractor")
+text = st.text_area("Enter Spanish text:", value="Estoy cansado de trabajar todos los días.")
+verbs = get_verbs(text)
+st.write("Subjunctive verbs:", verbs)

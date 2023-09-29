01ec2047e85d61f0f36729aa4ac3c927607ae2ff
morispolanco <morispolanco@gmail.com>
2023-09-29 01:42:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,22 @@
 import streamlit as st
 import spacy
+from spacy import displacy
 
-nlp= spacy.load("es_core_news_md")
+# Cargar el modelo de lenguaje de spaCy para español
+nlp = spacy.load("es_core_news_sm")
 
-st.title('Buscador de verbos en subjuntivo')
-
-text = st.text_area('Ingrese el texto a analizar:', height=200)
-
-if text:
+# Definir una función que tome un texto como entrada y devuelva una lista de verbos en modo subjuntivo
+def find_subjunctive_verbs(text):
     doc = nlp(text)
-    
-    verbs_in_subjunctive = []
-    
+    verbs = []
     for token in doc:
-        if token.pos_ == 'VERB' and token.morph.get('Mood') == 'Sub':
-            verbs_in_subjunctive.append(token.text)
-            
-    if verbs_in_subjunctive:
-        st.write('Se encontraron los siguientes verbos en subjuntivo:')
-        st.write(', '.join(verbs_in_subjunctive))
-    else:
-        st.write('No se encontraron verbos en subjuntivo.')
+        if token.pos_ == "VERB" and token.mood == "SUBJ":
+            verbs.append(token.text)
+    return verbs
+
+# Crear una interfaz de usuario con Streamlit para que el usuario pueda ingresar el texto y ver los verbos en modo subjuntivo
+st.title("Verbos en modo subjuntivo")
+text = st.text_input("Ingrese el texto:")
+verbs = find_subjunctive_verbs(text)
+st.write("Verbos en modo subjuntivo:")
+st.write(verbs)

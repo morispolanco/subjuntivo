5e90840cea932e2c89df657cab34bdb9f61a86a2
morispolanco <morispolanco@gmail.com>
2023-09-29 01:59:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,36 @@
 import streamlit as st
-import spacy
-from spacy import displacy
+from spaCy import displacy
 
-# Cargar el modelo de lenguaje de spaCy para español
+# Cargar el modelo de spaCy para el español
 nlp = spacy.load("es_core_news_sm")
 
-# Definir una función que tome un texto como entrada y devuelva una lista de verbos en modo subjuntivo
-def find_subjunctive_verbs(text):
-    doc = nlp(text)
-    verbs = []
-    for token in doc:
-        if token.pos_ == "VERB" and token.mood == "SUBJ":
-            verbs.append(token.text)
-    return verbs
+# Definir la función para buscar subjuntivos
+def buscar_subjuntivos(texto):
+    # Crear un objeto de spaCy para el texto
+    doc = nlp(texto)
+    # Buscar los tokens que son subjuntivos
+    subjuntivos = [token for token in doc if token.dep_ == "subj"]
+    # Devolver una lista con los subjuntivos encontrados
+    return subjuntivos
 
-# Crear una interfaz de usuario con Streamlit para que el usuario pueda ingresar el texto y ver los verbos en modo subjuntivo
-st.title("Verbos en modo subjuntivo")
-text = st.text_input("Ingrese el texto:")
-verbs = find_subjunctive_verbs(text)
-st.write("Verbos en modo subjuntivo:")
-st.write(verbs)
+# Definir la función para mostrar los subjuntivos en la interfaz de Streamlit
+def mostrar_subjuntivos(subjuntivos):
+    # Crear una tabla para mostrar los subjuntivos
+    table = st.table(subjuntivos)
+    # Mostrar la tabla en la interfaz de Streamlit
+    st.write(table)
+
+# Definir la función principal de la aplicación
+def main():
+    # Crear un objeto de Streamlit para la interfaz de usuario
+    st.title("Buscador de subjuntivos")
+    # Crear un campo de texto para ingresar el texto a analizar
+    texto = st.text_input("Ingrese el texto a analizar:")
+    # Crear un botón para ejecutar la función de buscar subjuntivos
+    st.button("Buscar subjuntivos", on_click=buscar_subjuntivos(texto))
+    # Mostrar los subjuntivos encontrados en la interfaz de Streamlit
+    mostrar_subjuntivos(buscar_subjuntivos(texto))
+
+# Ejecutar la función principal de la aplicación
+if __name__ == "__main__":
+    main()

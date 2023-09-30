ad29cae178b4384887da0bf61086de5e0b22053b
morispolanco <morispolanco@gmail.com>
2023-09-29 21:43:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,31 @@
 import streamlit as st
-import spacy
 
 def extract_subjunctive_verbs(text):
-    nlp = spacy.load('es_core_news_sm')
-    doc = nlp(text)
-    
     subjunctive_verbs = []
-    
-    for token in doc:
-        if token.pos_ == 'VERB' and 'Subj' in token.tag_:
-            subjunctive_verbs.append(token.text)
-            
+    sentences = text.split(".")
+    for sentence in sentences:
+        words = sentence.split()
+        for word in words:
+            if word.lower() in subjunctive_phrases:
+                subjunctive_verbs.append(words[words.index(word) + 1])
     return subjunctive_verbs
 
+subjunctive_phrases = [
+    "Espero que", "Deseo que", "Ojalá que", "Sugiero que", "Recomiendo que",
+    "Insisto en que", "Me alegra que", "Lamento que", "Es necesario que",
+    "Es importante que", "Es fundamental que", "Es imprescindible que",
+    "Es preferible que", "Es conveniente que", "Es urgente que", "Es posible que",
+    "Es probable que", "Es dudoso que", "Es increíble que", "Es sorprendente que",
+    "Es triste que", "Es justo que", "Es injusto que", "Es bueno que",
+    "Es malo que", "Es mejor que", "Es peor que", "Es necesario que",
+    "Es recomendable que", "Es válido que"
+]
 
-# Configuración de la página
-st.set_page_config(page_title="Extracción de Verbos en Subjuntivo", layout="wide")
+st.title("Extracción de verbos en modo subjuntivo")
 
-# Título y descripción de la aplicación
-st.title("Extracción de Verbos en Subjuntivo")
-st.markdown("""
-Esta aplicación extrae los verbos en subjuntivo presentes en un texto escrito 
-en español utilizando la biblioteca spaCy.
-""")
-
-# Área para ingresar el texto
-text_input = st.text_area("Ingrese el texto:", height=200)
-
-if st.button("Extraer"):
-    if len(text_input.strip()) > 0:
-        verbs = extract_subjunctive_verbs(text_input)
-        
-        if len(verbs) > 0:
-            # Mostrar los verbos encontrados
-            st.success(f"Se encontraron {len(verbs)} verbos en subjuntivo:")
-            for i, verb in enumerate(verbs):
-                st.write(f"{i+1}. {verb}")
-        else:
-            # Si no se encontraron verbos, mostrar mensaje informativo
-            st.info("No se encontraron verbos en subjuntivo.")
+text_input = st.text_area("Ingresa un texto:")
+if st.button("Extraer verbos en modo subjuntivo"):
+    subjunctive_verbs = extract_subjunctive_verbs(text_input)
+    st.write("Verbos en modo subjuntivo encontrados:")
+    for verb in subjunctive_verbs:
+        st.write("- " + verb)

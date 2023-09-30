c046c7a108510dc70bc2d6609f14f489eb2391e0
morispolanco <morispolanco@gmail.com>
2023-09-29 22:53:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,20 @@ def find_subjunctive_verbs(text):
     parsed_text = parse(text, lemmata=True)
     subjunctive_verbs = []
     for sentence in parsed_text.split():
-        for word, tag, _, lemma in sentence:
+        for i, (word, tag, _, lemma) in enumerate(sentence):
             if tag.startswith("VB") and "subj" in tag:
-                subjunctive_verbs.append(lemma)
+                if i > 0 and sentence[i-1][1] == "VB":
+                    subjunctive_verbs.append(lemma)
     return subjunctive_verbs
 
-text = "quiero que estudies y seas rico"
+def main():
+    st.title("Extracción de verbos en subjuntivo")
+    text = st.text_area("Ingrese un texto:")
+    if st.button("Extraer verbos en subjuntivo"):
+        subjunctive_verbs = find_subjunctive_verbs(text)
+        st.write("Verbos en subjuntivo encontrados:")
+        for verb in subjunctive_verbs:
+            st.write(verb)
 
-subjunctive_verbs = find_subjunctive_verbs(text)
-
-print("Verbos en subjuntivo encontrados:")
-for verb in subjunctive_verbs:
-    print(verb)
+if __name__ == "__main__":
+    main()

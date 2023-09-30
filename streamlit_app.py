c6365eb8eddba6b3f1caf7feb7e8b59729667d1a
morispolanco <morispolanco@gmail.com>
2023-09-29 21:50:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,42 @@
 import streamlit as st
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from nltk.stem import SnowballStemmer
 
-def extract_subjunctive_verbs(text):
+# Descargar los datos necesarios de NLTK
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('omw')
+
+# Definir una función para encontrar verbos en subjuntivo
+def find_subjunctive_verbs(text):
+    # Tokenizar el texto
+    tokens = word_tokenize(text, language='spanish')
+    
+    # Realizar etiquetado gramatical
+    pos_tags = nltk.pos_tag(tokens, lang='es')
+    
+    # Inicializar el stemmer
+    stemmer = SnowballStemmer('spanish')
+    
+    # Filtrar los verbos en subjuntivo
     subjunctive_verbs = []
-    sentences = text.split(".")
-    for sentence in sentences:
-        words = sentence.split()
-        for word in words:
-            if word.lower() in subjunctive_phrases:
-                subjunctive_verbs.append(words[words.index(word) + 1])
+    for word, pos in pos_tags:
+        if pos.startswith('V') and word.endswith('r') and stemmer.stem(word) != word:
+            subjunctive_verbs.append((word, pos))
+    
     return subjunctive_verbs
 
-subjunctive_phrases = [
-    "Espero que", "Deseo que", "Ojalá que", "Sugiero que", "Recomiendo que",
-    "Insisto en que", "Me alegra que", "Lamento que", "Es necesario que",
-    "Es importante que", "Es fundamental que", "Es imprescindible que",
-    "Es preferible que", "Es conveniente que", "Es urgente que", "Es posible que",
-    "Es probable que", "Es dudoso que", "Es increíble que", "Es sorprendente que",
-    "Es triste que", "Es justo que", "Es injusto que", "Es bueno que",
-    "Es malo que", "Es mejor que", "Es peor que", "Es necesario que",
-    "Es recomendable que", "Es válido que"
-]
+def main():
+    st.title("Extracción de Verbos en Subjuntivo")
+    text = st.text_area("Ingrese un texto en español:")
+    
+    if st.button("Buscar Verbos en Subjuntivo"):
+        subjunctive_verbs = find_subjunctive_verbs(text)
+        st.write("Verbos en Subjuntivo encontrados:")
+        for verb, pos in subjunctive_verbs:
+            st.write(f"{verb} ({pos})")
 
-st.title("Extracción de verbos en modo subjuntivo")
-
-text_input = st.text_area("Ingresa un texto:")
-if st.button("Extraer verbos en modo subjuntivo"):
-    subjunctive_verbs = extract_subjunctive_verbs(text_input)
-    st.write("Verbos en modo subjuntivo encontrados:")
-    for verb in subjunctive_verbs:
-        st.write("- " + verb)
+if __name__ == "__main__":
+    main()

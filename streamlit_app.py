6279012171d48afc15840d168c052aac01cd2270
morispolanco <morispolanco@gmail.com>
2023-09-30 00:22:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import docx2txt
 import re
+import csv
 
 def find_subjunctive_verbs(text):
     verbs = []
@@ -9,17 +11,30 @@ def find_subjunctive_verbs(text):
             verbs.append(words[i+1])
     return verbs
 
+def count_subjunctive_verbs(file_path):
+    text = docx2txt.process(file_path)
+    subjunctive_verbs = find_subjunctive_verbs(text)
+    return len(subjunctive_verbs)
+
 def main():
     st.title("Identificador de verbos en subjuntivo")
-    st.write("Ingrese un texto en español para identificar los verbos en subjuntivo:")
+    st.write("Cargue un archivo .docx para identificar los verbos en subjuntivo:")
     
-    text = st.text_area("Texto")
+    file = st.file_uploader("Cargar archivo .docx", type=["docx"])
     
-    if st.button("Identificar verbos en subjuntivo"):
-        subjunctive_verbs = find_subjunctive_verbs(text)
-        st.write("Verbos en subjuntivo encontrados:")
-        for verb in subjunctive_verbs:
-            st.write(verb)
+    if file is not None:
+        file_path = file.name
+        with open(file_path, "wb") as f:
+            f.write(file.getbuffer())
+        
+        num_verbs = count_subjunctive_verbs(file_path)
+        
+        st.write("Número de verbos en subjuntivo encontrados:", num_verbs)
+        
+        with open("verbos_subjuntivo.csv", "w", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Archivo", "Número de verbos en subjuntivo"])
+            writer.writerow([file_path, num_verbs])
 
 if __name__ == "__main__":
     main()

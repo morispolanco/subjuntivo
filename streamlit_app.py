ee12180c11cf8bb45dbd3d27fa43137e28b09335
morispolanco <morispolanco@gmail.com>
2023-10-01 16:53:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,30 @@
-import streamlit as st
-import docx2txt
 import re
-import csv
+import streamlit as st
 
-def find_subjunctive_verbs(text):
-    verbs = []
-    words = text.split()
-    for i in range(len(words) - 1):
-        if words[i] == "que" and words[i+1].endswith("s"):
-            verbs.append(words[i+1])
-    return verbs
+def identificar_verbos_subjuntivos(texto):
+    # Definir el patrón para la conjugación verbal en subjuntivo
+    patron = r"\b(?P<verbo>[a-zA-Zá-úÁ-ÚñÑ]+[aei]s{0,1}a{0,1}r{0,1})\b"
 
-def count_subjunctive_verbs(file_path):
-    text = docx2txt.process(file_path)
-    subjunctive_verbs = find_subjunctive_verbs(text)
-    return len(subjunctive_verbs)
+    # Encontrar todas las coincidencias del patrón en el texto
+    coincidencias = re.findall(patron, texto)
 
-def main():
-    st.title("Identificador de verbos en subjuntivo")
-    st.write("Cargue un archivo .docx para identificar los verbos en subjuntivo:")
-    
-    file = st.file_uploader("Cargar archivo .docx", type=["docx"])
-    
-    if file is not None:
-        file_path = file.name
-        with open(file_path, "wb") as f:
-            f.write(file.getbuffer())
-        
-        num_verbs = count_subjunctive_verbs(file_path)
-        
-        st.write("Número de verbos en subjuntivo encontrados:", num_verbs)
-        
-        with open("verbos_subjuntivo.csv", "w", newline="") as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(["Archivo", "Número de verbos en subjuntivo"])
-            writer.writerow([file_path, num_verbs])
+    # Filtrar las coincidencias que no son verbos (por ejemplo, "que")
+    verbos_subjuntivos = [coincidencia for coincidencia in coincidencias if coincidencia.lower() != "que"]
 
-if __name__ == "__main__":
-    main()
+    return verbos_subjuntivos
+
+# Título de la aplicación
+st.title("Identificador de Verbos en Subjuntivo")
+
+# Ingresar el texto
+texto = st.text_area("Ingrese el texto")
+
+# Botón para identificar los verbos en subjuntivo
+if st.button("Identificar"):
+    if texto:
+        verbos_subjuntivos = identificar_verbos_subjuntivos(texto)
+        st.write("Verbos en subjuntivo encontrados:")
+        for verbo in verbos_subjuntivos:
+            st.write(verbo)
+    else:
+        st.warning("Por favor, ingrese un texto antes de identificar los verbos en subjuntivo.")

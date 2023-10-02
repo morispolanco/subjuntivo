b102c67e31b8971d1bdab64b6119fb8e9df1601b
morispolanco <morispolanco@gmail.com>
2023-10-01 18:18:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,51 +5,49 @@ import re
 import base64
 
 # Título de la aplicación
-st.title("Extracción de verbos después de expresiones de esperanza o deseo")
+st.title("Extracción de expresiones de deseo y palabras siguientes")
 
 # Campo de carga de archivo .docx
 uploaded_file = st.file_uploader("Cargar archivo .docx", type="docx")
 
-# Función para extraer los verbos después de expresiones de esperanza o deseo
-def extraer_verbos(texto):
-    # Expresiones de esperanza o deseo en español
-    expresiones = ["quiero que", "espero que", "ojalá que", "deseo que",
-                   "espero que", "deseo que", "ojalá que", "quisiera que",
-                   "me gustaría que", "sería bueno que", "sería genial que",
-                   "sería fantástico que", "sería maravilloso que",
-                   "sería increíble que", "sería ideal que", "sería perfecto que",
-                   "sería estupendo que", "sería fabuloso que", "sería deseable que"]
-    
-    # Patrón regex para buscar las expresiones seguidas de un verbo
-    patron = r"(?i)(" + "|".join(expresiones) + r")\s+(\w+)"
+# Lista de expresiones de deseo en español
+expresiones = ["quiero que", "espero que", "ojalá que", "deseo que",
+               "espero que", "deseo que", "ojalá que", "quisiera que",
+               "me gustaría que", "sería bueno que", "sería genial que",
+               "sería fantástico que", "sería maravilloso que",
+               "sería increíble que", "sería ideal que", "sería perfecto que",
+               "sería estupendo que", "sería fabuloso que", "sería deseable que"]
+
+# Función para extraer las expresiones de deseo y las dos palabras siguientes
+def extraer_expresiones(texto):
+    # Patrón regex para buscar las expresiones de deseo y las dos palabras siguientes
+    patron = r"(?i)(" + "|".join(expresiones) + r")\s+(\w+)\s+(\w+)"
     
     # Buscar coincidencias en el texto
     coincidencias = re.findall(patron, texto)
     
-    # Crear un DataFrame con los verbos y su frecuencia
-    df = pd.DataFrame(coincidencias, columns=["Expresión", "Verbo"])
-    df["Frecuencia"] = df.groupby("Verbo")["Verbo"].transform("count")
-    df = df.drop_duplicates().reset_index(drop=True)
+    # Crear un DataFrame con las expresiones de deseo y las palabras siguientes
+    df = pd.DataFrame(coincidencias, columns=["Expresión", "Palabra 1", "Palabra 2"])
     
     return df
 
-# Botón para extraer los verbos y descargar el archivo CSV
-if st.button("Extraer verbos"):
+# Botón para extraer las expresiones y palabras y descargar el archivo CSV
+if st.button("Extraer expresiones y palabras"):
     if uploaded_file is not None:
         texto_docx = docx2txt.process(uploaded_file)
-        df_verbos = extraer_verbos(texto_docx)
+        df_expresiones = extraer_expresiones(texto_docx)
         
-        if not df_verbos.empty:
-            st.success("Se han extraído los verbos exitosamente.")
+        if not df_expresiones.empty:
+            st.success("Se han extraído las expresiones y palabras exitosamente.")
             st.markdown("### Resultado")
-            st.table(df_verbos)
+            st.table(df_expresiones)
             
             st.markdown("### Descargar archivo CSV")
-            csv = df_verbos.to_csv(index=False)
+            csv = df_expresiones.to_csv(index=False)
             b64 = base64.b64encode(csv.encode()).decode()
-            href = f'<a href="data:file/csv;base64,{b64}" download="verbos.csv">Descargar CSV</a>'
+            href = f'<a href="data:file/csv;base64,{b64}" download="expresiones.csv">Descargar CSV</a>'
             st.markdown(href, unsafe_allow_html=True)
         else:
-            st.warning("No se encontraron verbos después de las expresiones de esperanza o deseo.")
+            st.warning("No se encontraron expresiones de deseo y palabras siguientes.")
     else:
         st.warning("Por favor, carga un archivo .docx.")

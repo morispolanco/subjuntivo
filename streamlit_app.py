76c30bd12df9bcd61b5ce89f43586d8069c2a30d
morispolanco <morispolanco@gmail.com>
2023-10-01 18:25:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import re
 import base64
 
 # Título de la aplicación
-st.title("Extracción de expresiones de deseo y palabras siguientes")
+st.title("Extracción de oraciones con expresiones de deseo")
 
 # Campo de carga de archivo .docx
 uploaded_file = st.file_uploader("Cargar archivo .docx", type="docx")
@@ -18,36 +18,36 @@ expresiones = ["quiero que", "espero que", "ojalá que", "deseo que",
                "sería increíble que", "sería ideal que", "sería perfecto que",
                "sería estupendo que", "sería fabuloso que", "sería deseable que"]
 
-# Función para extraer las expresiones de deseo y las dos palabras siguientes
-def extraer_expresiones(texto):
-    # Patrón regex para buscar las expresiones de deseo y las dos palabras siguientes
-    patron = r"(?i)(" + "|".join(expresiones) + r")\s+(\w+)\s+(\w+)"
+# Función para extraer las oraciones con expresiones de deseo
+def extraer_oraciones(texto):
+    # Patrón regex para buscar las oraciones con expresiones de deseo
+    patron = r"(?i)([^.!?]*(" + "|".join(expresiones) + r")[^.!?]*)[.!?]"
     
     # Buscar coincidencias en el texto
     coincidencias = re.findall(patron, texto)
     
-    # Crear un DataFrame con las expresiones de deseo y las palabras siguientes
-    df = pd.DataFrame(coincidencias, columns=["Expresión", "Palabra 1", "Palabra 2"])
+    # Crear un DataFrame con las oraciones
+    df = pd.DataFrame(coincidencias, columns=["Oración"])
     
     return df
 
-# Botón para extraer las expresiones y palabras y descargar el archivo CSV
-if st.button("Extraer expresiones y palabras"):
+# Botón para extraer las oraciones y descargar el archivo CSV
+if st.button("Extraer oraciones"):
     if uploaded_file is not None:
         texto_docx = docx2txt.process(uploaded_file)
-        df_expresiones = extraer_expresiones(texto_docx)
+        df_oraciones = extraer_oraciones(texto_docx)
         
-        if not df_expresiones.empty:
-            st.success("Se han extraído las expresiones y palabras exitosamente.")
+        if not df_oraciones.empty:
+            st.success("Se han extraído las oraciones exitosamente.")
             st.markdown("### Resultado")
-            st.table(df_expresiones)
+            st.table(df_oraciones)
             
             st.markdown("### Descargar archivo CSV")
-            csv = df_expresiones.to_csv(index=False)
+            csv = df_oraciones.to_csv(index=False)
             b64 = base64.b64encode(csv.encode()).decode()
-            href = f'<a href="data:file/csv;base64,{b64}" download="expresiones.csv">Descargar CSV</a>'
+            href = f'<a href="data:file/csv;base64,{b64}" download="oraciones.csv">Descargar CSV</a>'
             st.markdown(href, unsafe_allow_html=True)
         else:
-            st.warning("No se encontraron expresiones de deseo y palabras siguientes.")
+            st.warning("No se encontraron oraciones con expresiones de deseo.")
     else:
         st.warning("Por favor, carga un archivo .docx.")
